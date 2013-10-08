33dba58b1503ea29c8d45e1a80e1f4a153c96e10
JM <jmuro0321@gmail.com>
2013-10-08 13:53:59 -0400

    Created memory.js - a hardware simulation of main memory.

---
+++ scripts/os/MMU.js
@@ -0,0 +1,25 @@
+/** A software simulation of a memory management unit. **/
+/** Created by Joe Muro on 9/30/13. **/
+
+function MMU(memory) {
+
+    this.memory = memory; // A reference to main memory hardware.
+    this.
+
+    this.write = function(address, byte) {
+
+    };
+
+    this.read = function(address, offset, nBytes) {
+
+    };
+
+    this.allocate = function(nBytes) {
+
+    };
+
+    this.deallocate = function(rangeStart, nBytes) {
+
+    };
+}
+
