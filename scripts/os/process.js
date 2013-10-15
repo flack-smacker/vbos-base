e72182e153578a9a06bfd506d1d40cc27440eb17
JM <jmuro0321@gmail.com>
2013-10-15 19:03:47 -0400

    Made several minor formatting changes.

--- scripts/os/process.js
+++ scripts/os/process.js
@@ -2,19 +2,19 @@
  * Created by Joe Muro on 10/7/13.
  */
 
-function ProcCtrlBlk(pid, priority) {
+function ProcCtrlBlk() {
 
     // PROCESS STATE INFORMATION
-    this.PID = -1 // the integer ID assigned to this process
-    this.Priority = -1 // the integer priority assigned to this process
-    this.State = ProcessStates.NEW // the current process state (i.e. running, waiting, terminated, etc.)
+    this.PID = null; // the integer ID assigned to this process
+    this.Priority = null;// the integer priority assigned to this process
+    this.State = ProcessState.NEW; // the current process state (i.e. running, waiting, terminated, etc.)
 
     // ADDRESS SPACE INFORMATION
-    this.BASE_ADDRESS = -1 // the first address in this process address space
-    this.LIMIT = -1 // the size of this processes address space
+    this.BASE_ADDRESS = null; // the first address in this process address space
+    this.LIMIT = null; // the size of this processes address space
 
     // CPU STATE INFORMATION
-    this.PC = 0 // contains the address of the next executable instruction
+    this.PC = 0; // contains the address of the next executable instruction
     this.Acc   = 0; // the value contained in the accumulator register
     this.Xreg  = 0; // the value contained in the X register
     this.Yreg  = 0; // the value contained in the Y register
@@ -22,7 +22,7 @@ function ProcCtrlBlk(pid, priority) {
 
 }
 
-ProcessStates = {
+ProcessState = {
 
     NEW: 0,
     RUNNING: 1,
